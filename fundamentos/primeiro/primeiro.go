0bbe6456d2689ea088aaca26a4685be9a3593208
extreeme <azvini@outlook.com>
2018-03-17 13:49:11 -0300

    primeiroPrograma

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,14 @@
+//Programas executaveis iniciam pelo pacote main
+package main
+
+// Os códigos em GO são organizados em pacotes
+// e para usá-los é necessário declarar um ou vários imports
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Print("Primeiro ")
+	fmt.Println("Programa")
+}
